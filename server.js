6bb21d26cfda39b5521b6cca589c3e1ccac12fd0
Maksim Vugnyavyy <maksim.vugnyavyy@gmail.com>
2017-05-08 01:29:18 +0300

    Edited all files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 
 // http://localhost:8080/
 app.get('/', (req, res) => {
-  res.sendfile('./index.html');
+  res.sendfile('index.html');
 });
 
 //Start server on the port 8080.
